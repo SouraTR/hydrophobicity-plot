4b8d4e383e98dde28a4aed0bb2f7bcc0f88a3a6a
SouraTR <51744901+SouraTR@users.noreply.github.com>
2020-03-31 23:06:32 +0530

    Update hydrophobicity.py

--- hydrophobicity.py
+++ hydrophobicity.py
@@ -1,16 +1,14 @@
 import matplotlib.pyplot as plt
 from scipy.ndimage.filters import gaussian_filter1d
 
-
+#dictionary with aminoacid code as key and hydrophobicity as corresponding values
 hyd = { 'A': 1.8,'R':-4.5,'N':-3.5,'D':-3.5,'C': 2.5,
        'Q':-3.5,'E':-3.5,'G':-0.4,'H':-3.2,'I': 4.5,
        'L': 3.8,'K':-3.9,'M': 1.9,'F': 2.8,'P':-1.6,
        'S':-0.8,'T':-0.7,'W':-0.9,'Y':-1.3,'V': 4.2 }
 
-
+#input protein sequence
 my_prot = str(input("Protein sequence: "))
-
-
 
 hydlist = []
 resnum = []
@@ -24,6 +22,7 @@ print(hydlist, resnum)
 plt.xlabel("Residue Number")
 plt.ylabel("Hydrophobicity")
 
+#smoothing plot using scipy -- I have no idea how this works I'll have to look into it
 ysmoothed = gaussian_filter1d(hydlist, sigma=2)
 plt.plot(resnum, ysmoothed)
 
